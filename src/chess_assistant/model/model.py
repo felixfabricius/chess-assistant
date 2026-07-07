ea36b36c9b03c556cad651ed68854f058c2bfd19
Felix Fabricius <felix@fabricius.in>
2026-07-07 16:52:25 +0100

    add: improve model architecture

--- src/chess_assistant/model/model.py
+++ src/chess_assistant/model/model.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 # We start with 144 x 144 x 4
 class SquareClassifier(nn.Module):
@@ -88,23 +87,12 @@ class SquareClassifier2(nn.Module):
     def __init__(self):
         super().__init__()
         """
-        Baseline: 
-        - downsample image after each convolutional layer
-        - downsample using max pool rather than convolutional with stride
-        - max pool and average pool at end
-        - add SOME info: 
-            - stuff about likely orientation of pieces.
-              so: what corner is at top; and perhaps somehow infer orientation based on 
-              the pixel coordinates?
-              This affects: how to interpret piece.
-            - But robot height also matters for this & makes pieces look differently.
-            - All of this can technically be captured via the pixel coordinates of corners & square.
-              And OHE version of what corner is at top.
-              So let's use this, and just take care to perhaps not repeat positions across different setups
-              (i.e. across different splits) too often!
-            - Then also: where to look for piece: mask 
+        Convolutional neural network that extracts features from images and then augments
+        with metadata; followed by fully connected layers.
 
-        To add: some normalisation? Batch norm / group norm etc?
+        Includes: batch normalisation, residual connections, depthwise dilated convolutional 
+        layers to widen receptive fields, and average and global pooling that preserves some
+        spatial structure in the end
         """
         # Downsample using max pool
         self.max_pool_1 = nn.MaxPool2d(
@@ -112,32 +100,52 @@ class SquareClassifier2(nn.Module):
             stride=2
         )
 
-        # Global max pool at end
-        self.max_pool_2 = nn.MaxPool2d(kernel_size=16)
-        # Global average pool at end
-        self.avg_pool_1 = nn.AvgPool2d(kernel_size=16)
-
         # ReLU
         self.relu = nn.ReLU()
 
         ### Image feature extraction
-        self.image_feature_extraction = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=32, kernel_size=3, padding=1),
+        self.image_feature_extraction_1 = nn.Sequential(
+            nn.Conv2d(in_channels=4, out_channels=32, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(32),
             self.relu,
             self.max_pool_1,
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64),
             self.relu, 
             self.max_pool_1, 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(128),
             self.relu,
-            self.max_pool_1,
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3),
-            nn.BatchNorm2d(256),
-            self.relu
+            self.max_pool_1
         )        
+        self.image_feature_extraction_2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            self.relu,
+            # Increase receptive field of neurons in final layer through a series
+            # of depthwise dilated convolutions, followed by a channel mix-in
+            nn.Conv2d(128, 128, kernel_size=3, groups=128, padding=1),
+            nn.BatchNorm2d(128),
+            self.relu,
+            nn.Conv2d(128, 128, kernel_size=3, groups=128, dilation=2, padding=2, bias=False),
+            nn.BatchNorm2d(128),
+            self.relu,
+            nn.Conv2d(128, 128, kernel_size=3, groups=128, dilation=5, padding=5, bias=False),
+            nn.BatchNorm2d(128),
+            self.relu,
+            # 1x1 Channel mix-in
+            nn.Conv2d(128, 128, kernel_size=1, groups=1, dilation=1, padding=0, bias=False),
+            nn.BatchNorm2d(128),
+        )
+        # Initialise the weights (scale) of the last batch norm to zero; so by default
+        # image_feature_extraction_2 is the identiy
+        nn.init.zeros_(self.image_feature_extraction_2[-1].weight)
+
+
+        # Adaptive Average Pooling at end; 2x2 to preserve SOME spatial structure
+        # Improvements to this would perhaps take the mask into account more explicitly
+        self.max_pool = nn.AdaptiveMaxPool2d((2, 2))
+        self.avg_pool = nn.AdaptiveAvgPool2d((2, 2))
 
         ### Preprocessing of additional info: small MLP
         self.bn1 = nn.BatchNorm1d(10)
@@ -148,22 +156,27 @@ class SquareClassifier2(nn.Module):
 
         ### Final MLP
         self.mlp_2 = nn.Sequential(
-            nn.Linear(528, 256),
+            nn.Linear(1024+16, 128),
             self.relu,
-            nn.Linear(256, 128),
+            nn.Linear(128, 64),
             self.relu,
-            nn.Linear(128, 13)
+            nn.Linear(64, 13)
         )
     
     def forward(self, image, metadata):
-        image_features = self.image_feature_extraction(image)
+        image_features = self.image_feature_extraction_1(image)
+        # Add a residual connection for this part, which is quite deep
+        image_features = self.relu(image_features + self.image_feature_extraction_2(image_features))
         image_features = torch.cat(
-            [
-                torch.flatten(nn.MaxPool2d(16)(image_features), start_dim=1), # want to maintain batch structure
-                torch.flatten(nn.AvgPool2d(16)(image_features), start_dim=1)
-            ],
+            (
+                torch.flatten(self.max_pool(image_features), start_dim=1),
+                torch.flatten(self.avg_pool(image_features), start_dim=1)
+            ),
             dim=1
         )
+        
+        # Shape of this will be 128 x 4
+
         # Batch norm is effective with batch size of 64 and shuffle=True in dataloader;
         # In that case, there will be data from multiple setups present
         # Potential issue: metadata varies only across the setups though, and there might be 
